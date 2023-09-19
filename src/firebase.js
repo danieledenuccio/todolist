d92f705fbae5a384867765b8ddcb72323349c9fe
danieledenuccio <111636061+danieledenuccio@users.noreply.github.com>
2023-09-19 11:24:12 +0200

    Update firebase.js
    Your personal Firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPznH5dCqNeYEmMR7lW27uOxdLCKtsEPg",
-    authDomain: "todolist-vue-135e2.firebaseapp.com",
-    projectId: "todolist-vue-135e2",
-    storageBucket: "todolist-vue-135e2.appspot.com",
-    messagingSenderId: "223563905734",
-    appId: "1:223563905734:web:eef3cbf208c12b99f575c7",
-    measurementId: "G-V6NQR4QXZR"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
   
 //   // Initialize Firebase
